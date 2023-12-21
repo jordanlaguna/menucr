3803fb025d79074a68a04cb12b8fe67f9f445718
AntonyValverde <gatovalverde4@gmail.com>
2023-12-21 17:42:22 -0600

    PrimaryCommit - Structures, colors, font in css and responsive in 70%

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,66 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main>
-      <h1 className="text-5xl font-bold mb-4">Hello World</h1>
-    </main>
+    <>
+      <div className="contenedorOne">
+        <header>
+          <h1>Restaurante XYZ</h1>
+          <p>Menú de Comidas</p>
+        </header>
+
+        <section>
+          <article>
+            <h2>Plato 1</h2>
+            <p>Descripción del plato 1. Incluye ingredientes y detalles.</p>
+            <p>Precio: $10.99</p>
+          </article>
+
+          <article>
+            <h2>Plato 2</h2>
+            <p>Descripción del plato 2. Incluye ingredientes y detalles.</p>
+            <p>Precio: $12.99</p>
+          </article>
+        </section>
+        <section>
+          <article>
+            <h2>Plato 1</h2>
+            <p>Descripción del plato 1. Incluye ingredientes y detalles.</p>
+            <p>Precio: $10.99</p>
+          </article>
+
+          <article>
+            <h2>Plato 2</h2>
+            <p>Descripción del plato 2. Incluye ingredientes y detalles.</p>
+            <p>Precio: $12.99</p>
+          </article>
+        </section>
+        <section>
+          <article>
+            <h2>Plato 1</h2>
+            <p>Descripción del plato 1. Incluye ingredientes y detalles.</p>
+            <p>Precio: $10.99</p>
+          </article>
+
+          <article>
+            <h2>Plato 2</h2>
+            <p>Descripción del plato 2. Incluye ingredientes y detalles.</p>
+            <p>Precio: $12.99</p>
+          </article>
+        </section>
+        <section>
+          <article>
+            <h2>Plato 1</h2>
+            <p>Descripción del plato 1. Incluye ingredientes y detalles.</p>
+            <p>Precio: $10.99</p>
+          </article>
+
+          <article>
+            <h2>Plato 2</h2>
+            <p>Descripción del plato 2. Incluye ingredientes y detalles.</p>
+            <p>Precio: $12.99</p>
+          </article>
+        </section>
+      </div>
+    </>
   );
 }
