09331ace2237789fdd8eecacfb905452525eac1c
Jordan Laguna <jordanlaguna10@gmail.com>
2024-04-01 15:31:49 -0600

    add space

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Article from "@/components/article/Article";
 import Footer from "@/components/footer/Footer";
 import Header from "@/components/head/Header";
 import Section from "@/components/section/Section";
+
 export default function Home() {
   return (
     <>
